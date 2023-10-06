a6674dd7c31aa286184f767f06046f4b72285b33
Tortik <147212106+Dohli-Trojan@users.noreply.github.com>
2023-10-06 23:43:38 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,2 +1,2 @@
 #Welcome! This code is written using procedural progrmaming language - python.
-#What this code does is described in 
+#What this code does is described in https://github.com/Dohli-Trojan/lemonade_stand/blob/1047a588905c0a273364173a3c6d0f30d109c164/README.md
